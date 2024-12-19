fad135cd67716a8fb341cf528a9786ebe77d8db8
youngbeen <pybju0@gmail.com>
2024-12-19 20:45:14 +0900

    Docs: 에러코드 삭제

--- project/frontend/src/types/images.d.ts
+++ project/frontend/src/types/images.d.ts
@@ -12,8 +12,3 @@ declare module '*.jpeg' {
   const value: string;
   export default value;
 }
-
-declare module '*.svg' {
-  const value: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-  export default value;
-}
